2a6a7e060a9f30e3fe5f4d67590f8900848ab4da
TimYi <yzdytm@163.com>
2015-09-26 00:36:56 +0800

    Update setup.py
    add BeautifilSoup4 requirement

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name="pyresttest3",
       keywords=['rest', 'web', 'http', 'testing', 'python3'],
       packages=['pyresttest3', 'pyresttest3.utils'],
       license='Apache License, Version 2.0',
-      requires=['jinja2', 'requests'],
+      requires=['jinja2', 'requests', 'BeautifulSoup4'],
       scripts=[],  # Make this executable from command line when installed
       provides=['pyresttest3']
       )
